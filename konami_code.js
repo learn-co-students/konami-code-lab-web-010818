5bd4df47c06d2270b6c03e362d9f20d975a05b15
Fei <fei.hafferkamp@gmail.com>
2018-02-25 21:56:36 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-
-  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
       index = 0;
       }
@@ -15,5 +14,4 @@ function init() {
       index = 0;
     }
   })
-
 }
